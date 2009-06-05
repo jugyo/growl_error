cef0a20c272c54d900be84534f03d6d6d9bf08a0
jugyo <kkohno@gmail.com>
2009-06-05 16:14:23 +0900

    first commit

---
+++ init.rb
@@ -0,0 +1,16 @@
+begin
+  require 'ruby-growl'
+
+  class ActionController::Base
+    GROWL = Growl.new "localhost", "growl_error", ["growl_error"]
+    def rescue_action_with_feature(exception)
+      GROWL.notify "growl_error", exception.class.name, exception.message
+    ensure
+      rescue_action_without_feature(exception)
+    end
+
+    alias_method_chain :rescue_action, :feature
+
+  end
+rescue LoadError => e
+end
